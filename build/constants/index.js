a3b1304ea728f845729f84e908444af28ab4d7ea
DCS97 <96494751+DCS97@users.noreply.github.com>
2022-02-08 00:32:14 +0000

    deleted node_modules

--- build/constants/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.FOLDER_PATH_THUMB_IMAGE = exports.FOLDER_PATH_FULL_IMAGE = void 0;
-const path_1 = __importDefault(require("path"));
-exports.FOLDER_PATH_FULL_IMAGE = `${path_1.default.resolve(__dirname, "../../Images/full")}`;
-exports.FOLDER_PATH_THUMB_IMAGE = `${path_1.default.resolve(__dirname, "../../Images/thumb")}`;
